d5500812ecd49c09fc1990d700bd3ca920ddfac0
Igor Silva <igor_tavares@outlook.com.br>
2025-08-20 17:17:24 +0200

    Added: New font design

--- App.tsx
+++ App.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { View, ActivityIndicator } from "react-native";
+import { useFonts } from "expo-font";
 import PrincipalView from "./screens/PrincipalView";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "WorkSans-Regular": require("./assets/fonts/WorkSans-Regular.ttf"),
+    "WorkSans-Bold": require("./assets/fonts/WorkSans-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
-      <PrincipalView />
+    //"Ready to shape your thoughts into words?"
+    <PrincipalView />
   );
 }
